9e58d71a62482c1cc8f9879cad3aa4ec26103d4e
pattersonq <pedrosa200051@gmail.com>
2021-11-10 16:01:25 +0100

    not much chaged

--- makefile
+++ makefile
@@ -2,9 +2,11 @@ PGDATABASE = si1
 PGUSER = alumnodb
 PGPASSWORD = alumnodb
 DBFILE = sql_files/dump_v1.4.sql.gz
-SQL_FILE = sql_files/actualiza.sql
+SQL_UPDATE_FILE = sql_files/actualiza.sql
+SQL_PRICE_FILE = sql_files/setPrice.sql
 
-reset_db: clear_db create_db update_db
+
+reset_db: clear_db create_db update_db set_price_db
 
 clear_db:
 	@echo Clear Database
@@ -15,6 +17,9 @@ create_db:
 	gunzip -c $(DBFILE) | psql $(PGDATABASE) $(PGUSER) 
 
 update_db:
-	psql $(PGDATABASE) $(PGUSER) -f $(SQL_FILE)
+	psql $(PGDATABASE) $(PGUSER) -f $(SQL_UPDATE_FILE)
+	
+set_price_db:
+	psql $(PGDATABASE) $(PGUSER) -f $(SQL_PRICE_FILE)
 
 	
--- sql_files/setPrice.sql
+++ sql_files/setPrice.sql
@@ -1,13 +1,12 @@
-
-UPDATE od
-SET price = 
+UPDATE orderdetail
+SET od.price = p.price * quantity * (1 - 1./51*(EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM od.orderdate)))
     -- Usamos 1/51 porque pasado un año el precio es el 102% del precio anterior
     -- Entonces 1/102*2 = 1/51, esto es mas exacto que usar 1/50
-    products.price * quantity * (1 - 1./51*(EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM orderdate)))
+FROM orderdetail od 
+INNER JOIN products p ON od.prod_id = p.prod_id
+INNER JOIN orders o ON od.orderid = o.orderid
+ORDER BY od.price;
 
-FROM  products, orders, orderdetail AS od
-WHERE orderdetail.prod_id = products.prod_id AND
-      orderdetail.orderid = orders.orderid;
 
 /* 
 -- Debug only 
